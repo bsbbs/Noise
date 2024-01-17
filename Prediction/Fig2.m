56601aff0796c8f8ea18c2b3b92d723fdbb6d7a3
Bo Shen NYU <bs3667@HAZ225-MBP14.fios-router.home>
2024-01-17 18:07:18 -0500

    Fig2

--- Prediction/Fig2.m
+++ Prediction/Fig2.m
@@ -16,13 +16,13 @@ addpath(genpath(Gitdir));
 filename = sprintf('Choice_MixedNoise');
 V1mean = 88;
 V2mean = 83;
-V3 = linspace(0, V1mean, 40)';
+V3 = linspace(0, V1mean, 50)';
 V1 = V1mean*ones(size(V3));
 V2 = V2mean*ones(size(V3));
 nsmpls = 1e7;
 
-epsvec = linspace(0, 9, 8);
-etavec = linspace(3.63, 0, 8);
+epsvec = linspace(0, 9, 8)/3;
+etavec = linspace(3.63, 0, 8)/3;
 
 % simulation
 matfile = fullfile(sim_dir, [filename, '.mat']);
@@ -48,7 +48,6 @@ end
 
 %% visualization
 h = figure; hold on;
-filename = 'Fig2a';
 gcolors = jet(9); %flip([winter(4); flip(autumn(4))]);
 mycols =[0.8472126105344099, 0.2612072279892349, 0.30519031141868513;
     0.9637831603229527, 0.47743175701653207, 0.28581314878892733;
@@ -78,7 +77,7 @@ plot([lt, lt], [max(ratio(:)), min(ratio(:))], 'k--');
 plot([rt, rt], [max(ratio(:)), min(ratio(:))], 'k--');
 xlabel('Scaled V3');
 ylabel('% Correct | V1, V2');
-mysavefig(h, filename, plot_dir, 12, [4, 4]);
+mysavefig(h, filename, plot_dir, 12, [5, 4]);
 %%
 h = figure; hold on;
 filename = 'Fig2a_Inset';
@@ -88,3 +87,57 @@ end
 ylabel('Slope');
 xlabel('Cases');
 mysavefig(h, filename, plot_dir, 12, [2, 2]);
+
+%% Full range panel
+filename = sprintf('Choice_MixedNoiseFullrng');
+V1mean = 88;
+V2mean = 83;
+V3vec = linspace(0, V1mean, 50);
+x = V3vec/V2mean;
+mask = x > .2 & x < .8;
+epsvec = linspace(0, 13, 201);
+[epsmesh, V3mesh] = meshgrid(epsvec, V3vec);
+V3 = V3mesh(:);
+eps = epsmesh(:);
+
+V1 = V1mean*ones(size(V3));
+V2 = V2mean*ones(size(V3));
+nsmpls = 1e7;
+etavec = linspace(0, 3.63*13/9, 201);
+
+matfile = fullfile(sim_dir, [filename, '.mat']);
+if ~exist(matfile, 'file')
+    slope = nan([numel(epsvec), numel(etavec)]);
+    for j = 1:numel(etavec)
+        fprintf("Eta #%i\n",j);
+        eta = etavec(j);
+        sdV1 = eps;
+        sdV2 = eps;
+        sdV3 = eps;
+        dat = table(V1,V2,V3,sdV1,sdV2,sdV3);
+        pars = [eta, 1, 1, 1];
+        tmp = dDNbFig2(pars, dat, nsmpls);
+        ratio = tmp(1,:)./(tmp(1,:) + tmp(2,:))*100;
+        ratio = reshape(ratio, numel(V3vec), numel(epsvec));
+        for i = 1:numel(epsvec)
+            coefficients = polyfit(x(mask), ratio(mask,i), 1);
+            slope(i,j) = coefficients(1);
+        end
+    end
+    save(matfile, 'slope');
+else
+    load(matfile);
+end
+%%
+addpath(genpath('/Users/bs3667/Library/CloudStorage/GoogleDrive-bs3667@nyu.edu/My Drive/Mylib'));
+h = figure;
+hold on;
+imagesc(etavec, epsvec, slope);
+colormap(bluewhitered);
+colorbar;
+ylabel('\sigma_{Early noise}');
+xlabel('\sigma_{Late noise}');
+xlim([min(etavec), max(etavec)]);
+ylim([min(epsvec), max(epsvec)]);
+mysavefig(h, filename, plot_dir, 12, [5, 4]);
+
