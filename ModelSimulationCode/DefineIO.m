7f8316e57216f82f772850a90a6321d9400c585f
bsbbs <boshen89@gmail.com>
2024-08-18 16:50:01 -0400

    hpc

--- ModelSimulationCode/DefineIO.m
+++ ModelSimulationCode/DefineIO.m
@@ -4,8 +4,8 @@ if strcmp(os,'MACI64')
     rootdir = '/Users/bs3667/Dropbox (NYU Langone Health)/Bo Shen Working files/NoiseProject';
     Gitdir = '~/Noise';
 elseif strcmp(os,'GLNXA64')
-    % rootdir = '/gpfs/data/glimcherlab/BoShen/NoiseProject';
-    rootdir = '/scratch/bs3667/NoiseProject';
+    rootdir = '/gpfs/data/glimcherlab/BoShen/NoiseProject';
+    % rootdir = '/scratch/bs3667/NoiseProject';
     Gitdir = '~/Noise';
 elseif strcmp(os, 'PCWIN64')
     rootdir = 'C:\Users\Bo\NYU Langone Health Dropbox\Shen Bo\Bo Shen Working files\NoiseProject';
