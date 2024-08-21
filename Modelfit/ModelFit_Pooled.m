be248ba383f25bea103b73af719db3ba7aa700e8
Bo Shen NYU <bs3667@HAZ225-MBP14.fios-router.home>
2024-08-21 18:35:35 -0400

    Model fit

--- Modelfit/ModelFit_Pooled.m
+++ Modelfit/ModelFit_Pooled.m
@@ -4,8 +4,8 @@ if strcmp(os,'MACI64')
     rootdir = '/Users/bs3667/Dropbox (NYU Langone Health)/Bo Shen Working files/NoiseProject';
     Gitdir = '~/Noise';
 elseif strcmp(os,'GLNXA64')
-    % rootdir = '/gpfs/data/glimcherlab/BoShen/NoiseProject';
-    rootdir = '/scratch/bs3667/NoiseProject';
+    rootdir = '/gpfs/data/glimcherlab/BoShen/NoiseProject';
+    % rootdir = '/scratch/bs3667/NoiseProject';
     Gitdir = '~/Noise';
 end
 addpath(genpath(Gitdir));
