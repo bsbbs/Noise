40e576fe17e2139ddd72056c0eacc700bb0ac82f
Bo Shen <40451542+bsbbs@users.noreply.github.com>
2025-01-06 16:25:47 -0500

    Update MeanScaledNoise.m

--- ModelSimulationCode/MeanScaledNoise.m
+++ ModelSimulationCode/MeanScaledNoise.m
@@ -96,6 +96,106 @@ ylabel('% Correct | V1, V2');
 xlim([0, V1mean]);
 mysavefig(h, filename, plot_dir, 12, [3, 8]);
 
+%% graded color, two panels
+V1mean = 88;
+V2mean = 83;
+eps1 = 4.5*2; % early noise for V1
+eps2 = 4.5*2; % early noise for V2
+V3 = linspace(0, V2mean, 50)';
+V3mean = mean(V3);
+eps3vec = linspace(0, .35*2, 6)*V2mean;
+V1 = V1mean*ones(size(V3));
+V2 = V2mean*ones(size(V3));
+sdV1 = eps1*ones(size(V3));
+sdV2 = eps2*ones(size(V3));
+etavec = [1, 1.4286]/4; % multiple levels of late noise
+K = 75;
+products = {'Probability'};
+for modeli = 1:4
+    filename = sprintf('Ratio_Model%i_%iv3max%1.0f_%s', modeli, numel(V3), max(V3), '6lines');
+    Rslts = table('Size', [0 4], 'VariableTypes', {'double', 'double', 'double', 'double'},...
+    'VariableNames', {'Early', 'Late', 'V3', 'choice'});
+    SimDatafile = fullfile(sim_dir, [filename, '.mat']);
+    % simulation
+    if exist(SimDatafile,'file')
+        load(SimDatafile);
+    else
+        fprintf('Simulate Model %i of 4\n', modeli);
+        switch modeli
+            case 1 % independent linear coding model, late noise
+                % controller = [(eps1+eps2)/2/(1+V1mean+V2mean+V3mean)*K, 0, 0, 1+V1mean+V2mean+V3mean]; 
+                controller = [0, 0, 0, 1+V1mean+V2mean+V3mean]; 
+                %  1. equivalence of early noise to late noise; 2. scaling for early noise; 3. weight of normalization; 4. baseline normalization
+            case 2 % independent linear coding model, early + late noise
+                controller = [0, 1, 0, 1+V1mean+V2mean+V3mean];
+            case 3 % divisive normalization model, late noise
+                % controller = [(eps1+eps2)/2/(1+V1mean+V2mean+V3mean)*K, 0, 1, 1];
+                controller = [0, 0, 1, 1];
+            case 4 % divisive normalization model, early + late noise
+                controller = [0, 1, 1, 1];
+        end
+        Ratios = nan(numel(eps3vec), numel(etavec), numel(V3));
+        for i = 1:numel(eps3vec)
+            fprintf('Early noise %i of %i\n', i, numel(eps3vec));
+            sdV3 = eps3vec(i)*ones(size(V3));
+            dat = table(V1,V2,V3,sdV1,sdV2,sdV3);
+            for ti = 1:numel(etavec)
+                fprintf('\tLate noise %i of 2', ti);
+                eta = etavec(ti);
+                pars = [ones(1,3)*(eta+controller(1)), controller(2:4)];
+                tmpb = nan([reps, numel(V3), 3]);
+                parfor ri = 1:reps
+                    [tmpb(ri,:,:), ~, ~] = dnDNM(dat, pars, 'biological', products); % biological model
+                    fprintf('.');
+                end
+                fprintf('\n');
+                probs = squeeze(mean(tmpb, 1));
+                Ratios(i,ti,:) = probs(:,1)./(probs(:,1) + probs(:,2))*100;
+            end
+        end
+        xval = V3'/V2mean;
+        save(SimDatafile, "Ratios","xval",'-mat');
+    end
+    for i = 1:numel(eps3vec)
+        for ti = 1:numel(etavec)
+            new_row = table(repmat(eps3vec(i),numel(V3),1), repmat(etavec(ti),numel(V3),1), V3, squeeze(Ratios(i,ti,:)),'VariableNames', Rslts.Properties.VariableNames);
+            Rslts = [Rslts; new_row];
+        end
+    end
+    writetable(Rslts, fullfile(sim_dir, [filename, '.txt']), 'Delimiter', '\t');
+
+    %% visualization
+    xval = V3'/V2mean;
+    lt = 0.2;
+    rt = 0.8;
+    mask = xval >= lt & xval <= rt;
+    cmap = jet(numel(eps3vec));
+    h = figure;
+    for ti = 1:numel(etavec)
+        if modeli == 4
+            subplot(1,2,ti);
+        end
+        hold on;
+        lg = [];
+        for i = 1:numel(eps3vec)
+            ratio = squeeze(Ratios(i, ti, :))';
+            lg(ti,i) = plot(xval, ratio, '-', 'LineWidth', 2, 'Color', cmap(i,:));
+        end
+        if ti == 1 || modeli == 4
+            plot([V1mean, V2mean]/V2mean, [1, 1]*min(ratio(:)), 'kv', 'MarkerFaceColor', [.7,.7,.7], 'MarkerSize', 5);
+            plot([lt, lt], [ylim], 'k--');
+            plot([rt, rt], [ylim], 'k--');
+            xlabel('Scaled V3');
+            ylabel('% Correct | V1, V2');
+        end
+        if modeli == 4
+            mysavefig(h, filename, plot_dir, 12, [7.27, 2.37]);
+        end
+    end
+    if modeli < 4
+        mysavefig(h, filename, plot_dir, 12, [7.27/2, 2.37]);
+    end
+end
 
 %% Test a cardinal view of V3 magnitude - V3 variance 
 % % filename = sprintf('V3mag100_var101_Choice_Ovlp');
